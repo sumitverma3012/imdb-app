86bd139d12eda0836bb4ecc1ba80a598fc97eb0f
Sumit V <sumit.v@logmein.com>
2021-05-08 21:02:53 +0530

    Updates

---
+++ precache-manifest.25553034a4be9970992609412066915a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aabe4fdb7031f21cc86",
+    "url": "/imdb-app/static/css/main.008b3eb3.chunk.css"
+  },
+  {
+    "revision": "1aabe4fdb7031f21cc86",
+    "url": "/imdb-app/static/js/main.0b03beab.chunk.js"
+  },
+  {
+    "revision": "1d213e12375024ef0652",
+    "url": "/imdb-app/static/js/runtime~main.2d4ac293.js"
+  },
+  {
+    "revision": "0f9ffc894b0bb5e35f22",
+    "url": "/imdb-app/static/js/2.898dcc32.chunk.js"
+  },
+  {
+    "revision": "9079f2bf9a097cffcd6ca9fbbeb39cbb",
+    "url": "/imdb-app/index.html"
+  }
+];
